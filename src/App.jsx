621dbe15039d55a281b3c44e9997d2edea0edb67
ombima56 <ombimahillary6@gmail.com>
2025-06-28 18:22:28 +0300

    feat: wrap AppContent in NavigationProvider for improved routing context

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "./components/ThemeProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { WatchlistProvider } from "./contexts/WatchlistContext";
+import { NavigationProvider } from "./contexts/NavigationContext";
 import {
   NetworkStatusProvider,
   OfflineIndicator,
@@ -54,31 +55,33 @@ function AppContent() {
 
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50 dark:bg-gray-900 flex flex-col">
-        <OfflineIndicator />
-        <Navbar />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/movie/:id" element={<MovieDetails />} />
-            <Route path="/tv/:id" element={<TVDetails />} />
-            <Route path="/watchlist" element={<Watchlist />} />
-            <Route path="/trending" element={<Trending />} />
-            {/* Catch-all route for 404 pages */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-        <Toaster
-          position="bottom-right"
-          toastOptions={{
-            style: {
-              background: "#333",
-              color: "#fff",
-            },
-          }}
-        />
-      </div>
+      <NavigationProvider>
+        <div className="min-h-screen bg-gray-50 dark:bg-gray-900 flex flex-col">
+          <OfflineIndicator />
+          <Navbar />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/movie/:id" element={<MovieDetails />} />
+              <Route path="/tv/:id" element={<TVDetails />} />
+              <Route path="/watchlist" element={<Watchlist />} />
+              <Route path="/trending" element={<Trending />} />
+              {/* Catch-all route for 404 pages */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+          <Toaster
+            position="bottom-right"
+            toastOptions={{
+              style: {
+                background: "#333",
+                color: "#fff",
+              },
+            }}
+          />
+        </div>
+      </NavigationProvider>
     </Router>
   );
 }
